8ebc48682ec8740b7b9d1fdebf387ffcf738532a
Ying-Wei Chen <e1023cindy@gmail.com>
2011-09-26 11:42:20 +0800

    first commit

---
+++ 0926_helloword/_926_hellowordAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  _926_hellowordAppDelegate.h
+//  0926_helloword
+//
+//  Created by App on 2011/9/26.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class _926_hellowordViewController;
+
+@interface _926_hellowordAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet _926_hellowordViewController *viewController;
+
+@end
---
+++ 0926_helloword/_926_hellowordViewController.h
@@ -0,0 +1,15 @@
+//
+//  _926_hellowordViewController.h
+//  0926_helloword
+//
+//  Created by App on 2011/9/26.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _926_hellowordViewController : UIViewController {
+    
+}
+
+@end
